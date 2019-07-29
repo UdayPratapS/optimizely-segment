be370a362cf87cd8910f74a35d6df2f59810a587
Uday <xgritanalytics@gmail.com>
2019-07-29 19:55:53 +0530

    Commit changes

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express();
 var request = require('request');
 
 var port = process.env.PORT;
-var port2 = process.env.TEST_PORT;
-var username = process.env.TOKEN;
+var port2 = process.env.TEST_PORT || 8084;
+//var username = process.env.TOKEN;
 
-//var username = "DiZcjKwEqXRF-DB6p77EQufVdzNLi78VnrEQKNdxKC9wBQTpaLiRQyvNKd-jZR_jHeNF291eneAiIm2S2jUPe2WLRULN69Kfv3bnShHEhOfk3nvMIIktEGgOBik8K_KjyzHSoim0Ph2WE33OWj-Ea__jQVuSQRtChrBn8rXmnXcqXZh9SiSaO7J1VY2EHa7puny5njhKAWep"
+var username = "DiZcjKwEqXRF-DB6p77EQufVdzNLi78VnrEQKNdxKC9wBQTpaLiRQyvNKd-jZR_jHeNF291eneAiIm2S2jUPe2WLRULN69Kfv3bnShHEhOfk3nvMIIktEGgOBik8K_KjyzHSoim0Ph2WE33OWj-Ea__jQVuSQRtChrBn8rXmnXcqXZh9SiSaO7J1VY2EHa7puny5njhKAWep"
 
 var password = '';
 var url = 'https://profiles.segment.com/v1/spaces/6lFVaWJT9K/collections/users/profiles/user_id:';
@@ -32,7 +32,7 @@ function main() {
         var personaId = req.query['personaId'];
         var verbose = req.query['verbose'];
 
-        //if(!personaId) personaId = "cDKAFE9zhjd2FyYn";
+        if(!personaId) personaId = "cDKAFE9zhjd2FyYn";
 
         if(personaId != null) {
             var completeURL = url + personaId + traits+"?limit=100";
@@ -44,16 +44,18 @@ function main() {
                 userDetails = result;
                 if(result['traits'])
                 cookieString = result['traits']['purchasers'];
-                //console.log(userDetails);
-                res.send(userDetails);
-                //purchaseraudience
+                //console.log("UserDetails", userDetails);
+                try{
+                    res.send(userDetails);
+                }catch(e){
+                    console.log("Cookies not sending", e);
+                }
             })
 
             //set the retrieved industry trait as a cookie.
             personasPromise.then(function () {
                 res.cookie('purchasers', cookieString);
                 res.sendfile('index.html');
-                //res.send('index.html');
             })
         } else{
             //For demo purposes we're just using a screen scraped copy of the Segment home page
